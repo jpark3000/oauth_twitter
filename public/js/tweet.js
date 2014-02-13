b3b34fe1f12613c3e18821626ec58cdf494c3ba1
Justin Park <jpark3000@gmail.com>
2014-02-12 18:47:04 -0600

    added more descriptive variable names in tweet.js " asdfsadhjf sdfl ; end

--- public/js/tweet.js
+++ public/js/tweet.js
@@ -1,12 +1,12 @@
 
-function getIt(data) {    
-  $.get('/status/' + data, function(data) {
-    console.log(data)
-    if (data == 'true') {  
+function getIt(jobId) {    
+  $.get('/status/' + jobId, function(sideKiqResponse) {
+    console.log(sideKiqResponse)
+    if (sideKiqResponse == 'true') {  
       $('#tweets').html('SUCCESS')
     } else {
       setTimeout(function() {
-          getIt(data);
+          getIt(jobId);
         }, 500);
     };
   }); 
@@ -24,12 +24,12 @@ $(document).ready(function() {
       $('#tweet-form submit').prop('disabled', true);
       $('#tweets').html('<img src="ajax-loader.gif">');
 
-      $.post('/tweet', tweet_data, function(data) {
+      $.post('/tweet', tweet_data, function(jobId) {
         $('#tweet-form input').prop('disabled', false);
         $('#tweet-form submit').prop('disabled', false);
         $('#tweet_input').val("");
 
-        getIt(data);
+        getIt(jobId);
 
       });
 
